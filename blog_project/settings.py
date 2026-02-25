16984b892402f77b0a540b0c1714907ae5e70e7e
Asad_Galib <152518383+Asad2114011@users.noreply.github.com>
2026-02-25 17:39:28 +0600

    Change STATICFILES_STORAGE to CompressedStaticFilesStorage

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -179,7 +179,7 @@ STATICFILES_DIRS=[
     BASE_DIR / 'static',
 ]                                                                            
 
-STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE='whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL='/media/'
 # MEDIA_ROOT=BASE_DIR / 'media'
